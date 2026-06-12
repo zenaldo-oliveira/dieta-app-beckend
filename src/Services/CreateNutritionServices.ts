5353711416a2dbedd3b49e20b074959fd622ce69
ZenaldoDev <zenaldi.221@hotmail.com>
2026-06-11 21:52:25 -0400

    update gemini model

--- src/Services/CreateNutritionServices.ts
+++ src/Services/CreateNutritionServices.ts
@@ -18,7 +18,7 @@ class CreateNutritionService {
       const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 
       const model = genAI.getGenerativeModel({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.5-flash",
       });
 
       const response = await model.generateContent(`
